def231e2756f869e43fe252876848950b2520968
Sree Gajula <gajulas@flip1.engr.oregonstate.edu>
2021-06-06 19:27:06 -0700

    tests

--- test_calculator.py
+++ test_calculator.py
@@ -1,20 +1,15 @@
+  
 import calculator
-
-
 
 
 class TestCalculatorApp:
 
 
-
-
     def test_add(self):
-        assert 5 == calculator.add(1, 4)
-
+        assert 5 == calculator.add(1, 4) 
 
     def test_subtract(self):
-        assert 2 == calculator.subtract(5, 3)
-
+        assert 2 == calculator.subtract(5, 3) 
 
     def test_multiply(self):
-        assert 10 == calculator.multiply(2, 5)
+        assert 10 == calculator.multiply(2, 5) 
