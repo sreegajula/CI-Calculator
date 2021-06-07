5b39ec0b46317728941a544d8a7f317c8f0f2ae7
Sree Gajula <gajulas@flip3.engr.oregonstate.edu>
2021-06-06 18:08:47 -0700

    added tests

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+import calculator
+
+
+class TestCalculatorApp:
+
+    def test_add(self):
+        assert 5 == calculator.add(1, 4)
+
+    def test_subtract(self):
+        assert 2 == calculator.subtract(5, 3)
+
+    def test_multiply(self):
+        assert 10 == calculator.multiply(2, 5)
