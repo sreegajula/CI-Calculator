02e70d7012754946a02a2871eecaad3875b18a26
Sree Gajula <gajulas@flip1.engr.oregonstate.edu>
2021-06-06 19:09:55 -0700

    tests

--- test_calculator.py
+++ test_calculator.py
@@ -3,6 +3,7 @@ import calculator
 
 class TestCalculatorApp:
 
+
     def test_add(self):
         assert 5 == calculator.add(1, 4)
 
@@ -11,3 +12,5 @@ class TestCalculatorApp:
 
     def test_multiply(self):
         assert 10 == calculator.multiply(2, 5)
+        
+        
